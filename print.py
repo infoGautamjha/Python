ac339508eb4757f6c190c13892dea6b0e0334a6a
infoGautamjha <infojhagautam@gmail.com>
2024-12-14 22:39:05 +0530

    Day1Python_print_Function_Pratice

---
+++ print.py
@@ -0,0 +1,24 @@
+print("Hello World")
+name = 'Kanak'
+age = 18
+print("Name:",name, "Age:",age)
+
+Name='DU'
+Age = 97
+Class= 'B.tech'
+Mob=6200
+print(Name, Class)
+a=15
+b=18
+sum=a+b;
+print(sum)
+print(a+b)
+print(18+15)
+
+print("Milk", "Apple","Gaurav",sep=", ")
+print("Gautam", "Jha", "Kanak")
+
+print("Gautam", end=" ")
+print("jha from Karanpur")
+
+print (f"Age:{age}, Name: {Name}")
