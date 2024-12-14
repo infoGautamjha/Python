82c26ee120a6f43651967ebb81b05ceee0806105
infoGautamjha <infojhagautam@gmail.com>
2024-12-14 10:31:10 +0530

    first commit

---
+++ demo.py
@@ -0,0 +1,27 @@
+a= 12
+b=13
+sum = a+b
+print(sum)
+name= 'Gautam Jha'
+age= 12
+city='jaipur'
+print(name ,age , city)
+
+#simple text output
+print("Hello Gautam Jha")
+
+#Printing Multiple Line
+#name = "Kanak LLp"
+#age = 15
+print("Name:",name, "Age:",age)
+
+#Seperation
+#Custom Seperation(sep)
+print("Apple","Banana","Milk", sep=", ")
+
+#Custon end Character (end)
+print("Hello", end=" ")
+print("world,kanak LLP!")
+
+#String formatting (F- strings)
+print(f"Age: {age}, Name: {name}")
