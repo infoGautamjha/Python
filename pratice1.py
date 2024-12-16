e86dc510497034c6a1f0ba0866d37125237db6b1
infoGautamjha <infojhagautam@gmail.com>
2024-12-16 09:50:12 +0530

    DAY3

---
+++ pratice1.py
@@ -0,0 +1,9 @@
+#Pratice Q
+
+#Wap to input user first name & print its length
+name = input("Enter your name : ")
+print("length of your name is", len(name))
+#WAP to find occurrence of $ in a string
+str = "hii,$ am the $123"
+print(str.count("$"))
+
