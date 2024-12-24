792b61f0e71d60830b4c1f6fbd29247f23a47acc
infoGautamjha <infojhagautam@gmail.com>
2024-12-24 14:27:05 +0530

    List

--- Liist.py
+++ Liist.py
@@ -28,9 +28,21 @@ office[1]= "Gautam"
 office[2]= "kanak"
 print(office)
 print(list.sort(office))
-'''
+
 b5="Hii, I am gautam From Karanpur which falls is madhubani Dist of Bihar"
 print(b5.replace("a", "n"))
 print(b5)
 b5=b5.replace("gautam", "Pradeep Jha")
 print(b5)
+'''
+office= [13,33,55,55,64,6,25,44]
+
+print(office)
+print(list.sort(office))
+print(type(office))
+print(office[3])
+office[0]= "Jha"
+office[1]= "Gautam"
+office[2]= "kanak"
+print(office)
+#print(list.sort(office))
